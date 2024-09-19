187e9285be0f2b8caf49ef63a0477015b275bf3c
Stephen Crowell <stephen.crowell@kitware.com>
2024-09-19 15:18:23 -0400

    Add test for metrics/metrics.py

---
+++ tests/metrics/test_metrics.py
@@ -0,0 +1,19 @@
+import pytest
+
+from pybsm.metrics import Metrics
+
+
+class TestMetrics:
+    @pytest.mark.parametrize(
+        ("name"),
+        [
+            ("Test"),
+        ],
+    )
+    def test_initialization(
+        self,
+        name: str,
+    ) -> None:
+        """Check if created metrics matches expected parameters."""
+        metrics = Metrics(name)
+        assert name == metrics.name
